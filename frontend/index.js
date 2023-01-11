db6897a73cbc21863f3e758266e95e2128263664
IuliiaSam <iuliia.samokhvalova@gmail.com>
2023-01-10 20:30:37 -0500

    frontend v1

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,12 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { render } from 'react-dom';
-import App from "./App";
-import Navbar from "./src/components/Navbar";
-import "./index.css";
-
-// https://github.com/facebook/react/issues/18866
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//     <Navbar />
-//   </React.StrictMode>
-// );
+import { render } from "react-dom";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
 
 render(
-  // wrap the App in the Provider Component and pass in the store
-    <App />, document.getElementById('root')
-)
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
